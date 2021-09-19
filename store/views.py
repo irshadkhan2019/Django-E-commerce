24a23e72dff351c369e934fc400cbd577bf0bbc8
Irshad <khanirshad2019@gmail.com>
2021-09-19 16:56:38 +0530

    search and pagination

--- store/views.py
+++ store/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,HttpResponse
 from .models import Product
 from category.models import Category
 from carts.models import Cart,CartItem
 from carts.views import _cart_id
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 # Create your views here.
 def store(request,category_slug=None):
     categories=None
@@ -11,13 +12,19 @@ def store(request,category_slug=None):
     if category_slug:
         categories=get_object_or_404(Category,slug=category_slug)
         products=Product.objects.filter(category=categories,is_available=True)
+        paginator=Paginator(products,2)
+        page=request.GET.get('page')
+        paged_product=paginator.get_page(page)
         product_count=products.count()
     else:
-        products=Product.objects.all().filter(is_available=True)
+        products=Product.objects.all().filter(is_available=True).order_by('id')
+        paginator=Paginator(products,2)
+        page=request.GET.get('page')
+        paged_product=paginator.get_page(page)
         product_count=products.count()
 
     context={
-    'products':products,
+    'products':paged_product,
     'product_count':product_count
     }
 
@@ -38,3 +45,18 @@ def product_detail(request,category_slug,product_slug):
     }
 
     return render(request,'store/product_detail.html',context)
+
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword=request.GET['keyword']
+        if keyword:
+            products=Product.objects.order_by('-created_date').filter(product_name__icontains=keyword)
+            product_count=products.count()
+
+    context={
+    'products':products,
+    'product_count':product_count
+    }
+    return render(request,'store/store.html',context)
